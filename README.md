bf4410625498d1d386b5123f9e64d7807a4299d3
solar0037 <solar0037@gmail.com>
2021-03-25 15:43:25 +0900

    update setup files

---
+++ README.md
@@ -0,0 +1,17 @@
+# arxiv-downloader
+
+arxiv-downloader is a command-line program to download papers from [arxiv.org](https://arxiv.org/).
+
+## Installation
+
+Download the latest release from [releases](https://github.com/solar0037/arxiv-downloader/releases) and install it with pip.
+
+```shell
+pip install arxiv_downloader-[version].whl
+```
+
+Alternatively, install from the repository.
+
+```shell
+pip install -e git+https://github.com/solar0037/arxiv-downloader#egg=arxiv-downloader
+```
--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='arxiv-downloader',
     version='0.1.dev1',
-    author='solar0037',
+    author='Hojun Sa',
     author_email='solar0037@gmail.com',
-    url='https://github.com/solar0037/arxiv-downloader',
+    url='https://github.com/solar0037/arxiv-downloader.git',
     description='Package to download papers on arxiv.org.',
+    project_urls={
+        "Bug Tracker": "https://github.com/solar0037/arxiv-downloader/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(),
+    python_requires=">=3.6",
     install_requires=[
         'bs4',
         'requests'
